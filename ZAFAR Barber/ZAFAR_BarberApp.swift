d4784f74cb775fc81b48ada3ecc01f9fc1fc9c0c
MIR1951 <160203220+MIR1951@users.noreply.github.com>
2025-07-23 10:35:50 +0500

    Initial Commit

---
+++ ZAFAR Barber/ZAFAR_BarberApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZAFAR_BarberApp.swift
+//  ZAFAR Barber
+//
+//  Created by Kenjaboy Xajiyev on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ZAFAR_BarberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZAFAR BarberTests/ZAFAR_BarberTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZAFAR_BarberTests.swift
+//  ZAFAR BarberTests
+//
+//  Created by Kenjaboy Xajiyev on 23/07/25.
+//
+
+import Testing
+@testable import ZAFAR_Barber
+
+struct ZAFAR_BarberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
